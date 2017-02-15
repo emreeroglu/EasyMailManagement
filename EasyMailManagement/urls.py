18447425176beb00071446f667704cbd8cf3c22f
Emre Eroglu <emre@emreeroglu.net>
2017-02-15 22:08:33 +0300

    Initial web app integration.

--- EasyMailManagement/urls.py
+++ EasyMailManagement/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
+from web import views
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', views.index, name='index'),
+)
