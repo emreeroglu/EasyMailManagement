edfe5c40f812b3364ae76dbdc17a027e42118337
Emre Eroglu <emre@emreeroglu.net>
2017-02-16 21:41:44 +0300

    Base command set implementation.

--- sysadmin/models.py
+++ sysadmin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class OperatingSystem(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=30)
+
+
+class CommandSet(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=30)
+    operating_system = models.ForeignKey(OperatingSystem)
+
+
+class Command(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=30)
+    command = models.CharField(blank=False, null=False, max_length=1000)
+    command_set = models.ForeignKey(CommandSet)
