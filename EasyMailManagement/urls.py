9fbc6e516452a9ecd56e8f0223810112eaa27d65
Emre Eroglu <emre@emreeroglu.net>
2017-03-03 03:57:52 +0300

    WIP fix for post request from html form.

--- EasyMailManagement/urls.py
+++ EasyMailManagement/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from web import views
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.login_page, name='login'),
     url(r'^logout/$', views.logout_page, name='logout'),
     url(r'^register/$', views.register_page, name='register'),
     url(r'^doregister/$', views.doregister, name='doregister'),
     url(r'^$', views.index, name='index'),
-)
+]
