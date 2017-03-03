79aef1a290ed7ba30c15a0f39a05606fc9a05c18
Emre Eroglu <emre@emreeroglu.net>
2017-03-03 19:42:48 +0300

    Localized urls.

--- EasyMailManagement/urls.py
+++ EasyMailManagement/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from web import views
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.login_page, name='login'),
     url(r'^logout/$', views.logout_page, name='logout'),
     url(r'^register/$', views.register_page, name='register'),
     url(r'^$', views.index, name='index'),
-]
+)
