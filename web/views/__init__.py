aa4bed4729bb61e5729ec041c7c372f14ff16a16
Emre Eroglu <emre@emreeroglu.net>
2017-03-04 20:56:13 +0300

    Add ajax check for username.

--- web/views/__init__.py
+++ web/views/__init__.py
@@ -1,6 +1,6 @@
 from .index import index
 from .login import login_page
 from .logout import logout_page
-from .register import register_page
+from .register import register_page, check_username
 
-__all__ = ['index', 'login_page', 'login_required','register_page', 'logout_page', ]
+__all__ = ['index', 'login_page', 'login_required', 'register_page', 'check_username', 'logout_page', ]
--- web/views/register.py
+++ web/views/register.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.utils.translation import ugettext_lazy as _
 
+from common.models import User
 from common.forms import RegisterForm
 from django.contrib.auth.hashers import make_password
 
@@ -22,3 +25,13 @@ def register_page(request):
         return render(request, 'web/register.html', {'form': form})
     form = RegisterForm()
     return render(request, 'web/register.html', {'form': form})
+
+
+def check_username(request):
+    username = request.GET.get('username', None)
+    data = {
+        'is_taken': User.objects.filter(username__iexact=username).exists()
+    }
+    if data['is_taken']:
+        data['error_message'] = _('A user with this username already exists.')
+    return JsonResponse(data)
