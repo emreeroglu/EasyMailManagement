9c0b17f5c8d5f5cba7e21483fe5e8305a8ee7aa2
Emre Eroglu <emre@emreeroglu.net>
2017-03-22 04:01:24 +0300

    Add new apps for better project structure.

--- EasyMailManagement/settings.py
+++ EasyMailManagement/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server',
+    'operatingsystem',
+    'command',
     'common',
     'web',
 ]
--- server/models.py
+++ server/models.py
@@ -3,19 +3,3 @@ from django.db import models
 
 class Server(models.Model):
     name = models.CharField(blank=False, null=False, max_length=30)
-    operating_system = models.ForeignKey(OperatingSystem)
-
-
-class OperatingSystem(models.Model):
-    name = models.CharField(blank=False, null=False, max_length=30)
-
-
-class CommandSet(models.Model):
-    name = models.CharField(blank=False, null=False, max_length=30)
-    operating_system = models.ForeignKey(OperatingSystem)
-
-
-class Command(models.Model):
-    name = models.CharField(blank=False, null=False, max_length=30)
-    command = models.CharField(blank=False, null=False, max_length=1000)
-    command_set = models.ForeignKey(CommandSet)
