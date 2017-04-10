054af233a335feb400049ffaef54642d6d880d0a
Emre Eroglu <emre@emreeroglu.net>
2017-04-10 18:29:02 +0300

    Got fabric working.

--- web/views/index.py
+++ web/views/index.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from fabfile import install_sudo
 
 def index(request):
     """Index Page."""
+    install_sudo()
     return render(request, "web/index.html", {})
