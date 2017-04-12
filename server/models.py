ec31aa668f878a0b4cc3f60b58fcb02806a7f69e
Emre Eroglu <emre@emreeroglu.net>
2017-04-12 06:34:11 +0300

    WIP - Add auto server management.

--- server/models.py
+++ server/models.py
@@ -6,5 +6,10 @@ class Server(models.Model):
     name = models.CharField(blank=False, null=False, max_length=30)
     ip = models.GenericIPAddressField(blank=True, null=True)
 
-    def setup_server(self):
+    def find_available_server(self):
         pass
+
+    def setup_server(self):
+        debian = Debian()
+        debian.ssh_copy_id()
+
