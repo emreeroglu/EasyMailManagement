87cfe6ce32304fbf25dbd27205b17a6dedc7c039
Emre Eroglu <emre@emreeroglu.net>
2017-04-18 14:54:42 +0300

    Personal and Corporate pages added.

--- web/views/__init__.py
+++ web/views/__init__.py
@@ -3,5 +3,8 @@ from .login import login_page
 from .logout import logout_page
 from .register import register_page, check_username
 from .servers import servers
+from .personal import personal
+from .corporate import corporate
 
-__all__ = ['index', 'login_page', 'login_required', 'register_page', 'check_username', 'logout_page', 'servers']
+__all__ = ['index', 'login_page', 'login_required', 'register_page', 'check_username', 'logout_page', 'servers',
+           'personal', 'corporate']
