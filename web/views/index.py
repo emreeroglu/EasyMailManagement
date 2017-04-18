f7cfed2ea6dfa5b72637d01bbfef24902a7121b4
Emre Eroglu <emre@emreeroglu.net>
2017-04-18 14:56:37 +0300

    Add empty line for PEP6 standard.

--- web/views/index.py
+++ web/views/index.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from fabfile import install_sudo
 from server.models import Server
 
+
 def index(request):
     """Index Page."""
     return render(request, "web/index.html", {})
