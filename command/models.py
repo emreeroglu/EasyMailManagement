3637596fcb7930a4b98c25b1624e9ecba2d61258
Emre Eroglu <emre@emreeroglu.net>
2017-05-28 19:31:30 +0300

    Add description.

--- command/models.py
+++ command/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Command(models.Model):
     name = models.CharField(blank=False, null=False, max_length=30)
-    command = models.CharField(blank=False, null=False, max_length=1000)
+    command = models.CharField(blank=False, null=False, max_length=256)
+    description = models.CharField(blank=True, null=True, max_length=1000)
