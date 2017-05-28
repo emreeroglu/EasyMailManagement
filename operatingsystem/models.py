9398073f04e6ca2423d257cdcc977070b5dcc052
Emre Eroglu <emre@emreeroglu.net>
2017-05-28 19:31:44 +0300

    Add command and command set.

--- operatingsystem/models.py
+++ operatingsystem/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from server.models import Server
+from command.models import Command
+from commandset.models import CommandSet
 
 
 class OperatingSystem(models.Model):
     name = models.CharField(blank=False, null=False, max_length=30)
     server = models.ForeignKey(Server, blank=True, null=True)
+    command = models.ForeignKey(Command, blank=True, null=True)
+    command_set = models.ForeignKey(CommandSet, blank=True, null=True)
